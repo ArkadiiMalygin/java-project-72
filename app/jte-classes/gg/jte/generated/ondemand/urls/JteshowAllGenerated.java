f99ec934138231b88bfe0147f7fecf0fc3e3ecdb
Arkadii <arkadii.malygin@gmail.com>
2024-10-19 02:19:13 +0300

    work 3 extension

---
+++ app/jte-classes/gg/jte/generated/ondemand/urls/JteshowAllGenerated.java
@@ -0,0 +1,29 @@
+package gg.jte.generated.ondemand.urls;
+import hexlet.code.util.NamedRoutes;
+import hexlet.code.dto.urls.UrlsPage;
+public final class JteshowAllGenerated {
+	public static final String JTE_NAME = "urls/showAll.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,4,4,6,6,12,12,15,15,15,18,18,18,21,21,25,25,25,25,25,2,2,2,2};
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, UrlsPage page) {
+		jteOutput.writeContent("\r\n");
+		gg.jte.generated.ondemand.layout.JtepageGenerated.render(jteOutput, jteHtmlInterceptor, new gg.jte.html.HtmlContent() {
+			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
+				jteOutput.writeContent("\r\n    <div class=\"mx-auto p-4 py-md-5\">\r\n        <div>\r\n            <h1>All urls</h1>\r\n\r\n            <table class=\"table table-striped\">\r\n                ");
+				for (var url : page.getUrls()) {
+					jteOutput.writeContent("\r\n                    <tr>\r\n                        <td>\r\n                            ");
+					jteOutput.setContext("td", null);
+					jteOutput.writeUserContent(url.getId());
+					jteOutput.writeContent("\r\n                        </td>\r\n                        <td>\r\n                            ");
+					jteOutput.setContext("td", null);
+					jteOutput.writeUserContent(url.getName());
+					jteOutput.writeContent("\r\n                        </td>\r\n                    </tr>\r\n                ");
+				}
+				jteOutput.writeContent("\r\n            </table>\r\n        </div>\r\n    </div>\r\n");
+			}
+		});
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		UrlsPage page = (UrlsPage)params.get("page");
+		render(jteOutput, jteHtmlInterceptor, page);
+	}
+}

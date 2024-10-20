34e80a13711199476eddf474f1006ec086aeee44
Arkadii <arkadii.malygin@gmail.com>
2024-10-21 02:18:34 +0300

    jacoco added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     application
     checkstyle
+    jacoco
     id("io.freefair.lombok") version "8.6"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -46,4 +47,9 @@ tasks.test {
         // showCauses = true
         showStandardStreams = true
     }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports { xml.required.set(true) }
 }

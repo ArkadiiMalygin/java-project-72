33e8aecf3fefd725afc38a25e3b18dc9d85d6e1c
Arkadii <arkadii.malygin@gmail.com>
2024-10-21 17:54:03 +0300

    no cascade in right order

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,5 +1,13 @@
 DROP TABLE IF EXISTS url_checks;
 
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE urls (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+
 CREATE TABLE url_checks (
     id INT PRIMARY KEY AUTO_INCREMENT,
     url_id INT REFERENCES urls(id) NOT NULL,
@@ -7,13 +15,5 @@ CREATE TABLE url_checks (
     h1 VARCHAR(255),
     title VARCHAR(255),
     description TEXT,
-    created_at TIMESTAMP
-);
-
-DROP TABLE IF EXISTS urls;
-
-CREATE TABLE urls (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP
 );
